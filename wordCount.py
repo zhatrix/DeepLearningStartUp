8ce852c8644de4b45aa2fc865369900a85a87ecf
zhatrix <zhatrix@gmail.com>
2017-02-22 08:21:00 +0800

    fix

--- wordCount.py
+++ wordCount.py
@@ -8,7 +8,7 @@ wordList = content.split(" ")
 # 生成一个计数的字典
 c = Counter()
 # 把满足条件的二元词组放入计数字典中，并计数
-for i in range(1,(len(wordList) - 1)) :
+for i in range(0,(len(wordList) - 1)) :
     if len(wordList[i]) == 2 and len(wordList[i + 1]) == 2:
         double_words = wordList[i] + wordList[i + 1]
         c[double_words] = c[double_words] + 1
